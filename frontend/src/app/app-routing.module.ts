bb622357bbf188343e5d608eab2be704fc37c707
Josselyn <vanesitapolanco@gmail.com>
2021-12-06 13:46:04 -0600

    agregando librerias en frontend

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { PrincipalComponent } from './principal/principal.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'principal',
+    component:PrincipalComponent
+  },
+  {
+    path: '',
+    redirectTo: '/principal',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
