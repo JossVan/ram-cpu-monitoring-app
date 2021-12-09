97b49d32825e97d74fb308ad7e4a569e9791ebc6
jossvan <vanesitapolanco@gmail.com>
2021-12-09 14:28:25 -0600

    modulo de ram

--- backend/modulos/memo_201602676.c
+++ backend/modulos/memo_201602676.c
@@ -1,70 +1,78 @@
-//Header obligatorio de todos los modulos
 #include <linux/module.h>
-//Header para usar KERN_INFO
-#include <linux/kernel.h>
-
-//Header para los macros module_init y module_exit
 #include <linux/init.h>
-//Header necesario porque se usara proc_fs
+#include <linux/kernel.h>
+#include <linux/fs.h>
 #include <linux/proc_fs.h>
-/* for copy_from_user */
-#include <asm/uaccess.h>	
-/* Header para usar la lib seq_file y manejar el archivo en /proc*/
 #include <linux/seq_file.h>
+#include <asm/uaccess.h>
+#include <linux/hugetlb.h>
 
+MODULE_DESCRIPTION("módulo que lee la RAM del sistema");
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("Módulo de RAM");
-MODULE_AUTHOR("Josselyn Vanessa Polanco Gameros");
+MODULE_AUTHOR("Josselyn Polanco");
+struct sysinfo info;
+unsigned long cached;
+unsigned long memorytotal;
+double a;
+unsigned long pages[NR_LRU_LISTS];
+int lru;
 
-//Funcion que se ejecutara cada vez que se lea el archivo con el comando CAT
-static int escribir_archivo(struct seq_file *archivo, void *v)
-{   
+static int Escribir_Archivo(struct seq_file *archivo, void *v)
+{
+    #define K(x) ((x) << (PAGE_SHIFT - 10))
+        si_meminfo(&info);
+        cached = global_node_page_state(NR_FILE_PAGES) -
+                global_node_page_state(QC_SPACE) - info.bufferram;
+        if (cached < 0)
+            cached = 0;
+        for (lru = LRU_BASE; lru < NR_LRU_LISTS; lru++)
+            pages[lru] = global_node_page_state(NR_LRU_BASE + lru);
 
-    seq_printf(archivo, "*********************************************\n");
-    seq_printf(archivo, "*********************************************\n");
-    seq_printf(archivo, "**    LABORATORIO SISTEMAS OPERATIVOS 1    **\n");
-    seq_printf(archivo, "**                Proyecto 1               **\n");
-    seq_printf(archivo, "**    JOSSELYN VANESSA POLANCO GAMEROS     **\n");
-    seq_printf(archivo, "*********************************************\n");
-    seq_printf(archivo, "*********************************************\n");
-    return 0;
+        seq_printf(archivo, "%8lu\n", K(info.totalram)); // memoria total
+        seq_printf(archivo, "%8ld\n", ((K(info.totalram) - (K(info.freeram) + K(info.bufferram) + K(cached))))); // memoria en uso
+        seq_printf(archivo, "%8ld\n", (K(info.freeram))); // memoria libre
+        seq_printf(archivo, "%8ld\n", (K(cached))); //caché
+        seq_printf(archivo, "%8ld\n", (K(info.bufferram))); // buffer
+        seq_printf(archivo, "%8ld", (((K(info.totalram) - (K(info.freeram) + K(info.bufferram) + K(cached))) * 100) / (K(info.totalram)))); //porcentaje de memoria en uso
+
+    #undef K
+	return 0;
 }
 
-//Funcion que se ejecutara cada vez que se lea el archivo con el comando CAT
-static int al_abrir(struct inode *inode, struct file *file)
+static int abrir(struct inode *inode, struct file *file)
 {
-    return single_open(file, escribir_archivo, NULL);
+	return single_open(file, Escribir_Archivo, NULL);
 }
 
-static struct proc_ops operaciones =
-{
-    .proc_open = al_abrir,
-    .proc_read = seq_read
+static const struct proc_ops operaciones = {
+	.proc_open = abrir,
+	.proc_read = seq_read,
+	.proc_lseek = seq_lseek,
+	.proc_release = single_release,
 };
 
-
-//Funcion a ejecuta al insertar el modulo en el kernel con insmod
-static int _insert(void)
+static int __init insert(void)
 {
-    proc_create("memo_201602676", 0, NULL, &operaciones);
-    printk(KERN_INFO "***** Proyecto 1 *****\n");
+	printk(KERN_INFO "Cargando modulo.\r\n");
+	proc_create("memo_201602676", 0, NULL, &operaciones);
+	printk(KERN_INFO "***** Proyecto 1 *****\n");
     printk(KERN_INFO "Nombre: Josselyn Vanessa Polanco Gameros");
     printk(KERN_INFO "Carnet: 201602676");
     printk(KERN_INFO "Insertando el módulo para información de RAM\n");
     
-    return 0;
+	return 0;
 }
 
-//Funcion a ejecuta al remover el modulo del kernel con rmmod
-static void _remove(void)
+static void __exit remove(void)
 {
-    remove_proc_entry("memo_201602676", NULL);
-    printk(KERN_INFO "***** Proyecto 1 *****\n");
+	remove_proc_entry("memo_201602676", NULL);
+	printk(KERN_INFO "***** Proyecto 1 *****\n");
     printk(KERN_INFO "Curso: SISTEMAS OPERATIVOS I");
     printk(KERN_INFO "Nombre: Josselyn Vanessa Polanco Gameros");
     printk(KERN_INFO "Carnet: 201602676");
     printk(KERN_INFO "Removiendo el módulo para información de RAM\n");
 }
 
-module_init(_insert);
-module_exit(_remove);
+module_init(insert);
+module_exit(remove);
+
