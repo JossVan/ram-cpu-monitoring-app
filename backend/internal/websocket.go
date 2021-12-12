75ee1a9465a885ef28ae44cae80daf36d89d50a9
jossvan <vanesitapolanco@gmail.com>
2021-12-11 18:44:19 -0600

    empezando websockets

--- backend/internal/websocket.go
+++ backend/internal/websocket.go
@@ -1,5 +1,55 @@
-package internal
+package main
 
-import "github.com/gorilla/websocket"
+import (
+	//"log"
+	"fmt"
+	"log"
+	"net/http"
 
-var upGradeWebSocket = websocket.Upgrade{}
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func reader(conn *websocket.Conn) {
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		log.Println(string(p))
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+		}
+	}
+}
+func inicio(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "inicio")
+}
+func infoRam(w http.ResponseWriter, r *http.Request) {
+	upgrader.CheckOrigin = func(r *http.Request) bool {
+		return true
+	}
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print(err)
+	} else {
+		log.Print(ws)
+	}
+	reader(ws)
+}
+func setupRoutes() {
+	http.HandleFunc("/", inicio)
+	http.HandleFunc("/RAM", infoRam)
+}
+
+func main() {
+	setupRoutes()
+	fmt.Println("buenas...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
