fc3dbda6e453d7ed5ed96c05892ce0fef9a353b1
Josselyn <vanesitapolanco@gmail.com>
2021-12-12 17:00:11 -0600

    arreglando tabla de procesos

--- frontend/src/app/arbolproceso/arbolproceso.component.ts
+++ frontend/src/app/arbolproceso/arbolproceso.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { WebsocketService } from '../websocket.service';
 import { interval } from 'rxjs';
 import { takeWhile } from 'rxjs/operators';
+import * as jQuery from 'jquery';
+
 @Component({
   selector: 'app-arbolproceso',
   templateUrl: './arbolproceso.component.html',
@@ -12,15 +14,70 @@ export class ArbolprocesoComponent implements OnInit {
   constructor(private servicio: WebsocketService) { }
   info : any[] = [];
   ngOnInit(): void {
-    this.actualizar()
-    /*interval(1000)
+    interval(1000)
     .pipe(takeWhile(() => true))
     .subscribe(() => {
     this.actualizar()
-    })*/
+    let codigo = "<table class=\"footable table table-stripped toggle-arrow-tiny\" data-page-size=\"15\">"+
+    "<thead>"+
+    "<tr>"+
+
+        "<th data-toggle=\"true\">PID</th>"+
+        "<th data-hide=<\"phone\">Nombre del proceso</th>"+
+        "<th data-hide=\"all\">Procesos hijos</th>"+
+        "<th data-hide=\"phone\">Usuario</th>"+
+        "<th data-hide=\"phone\" > RAM utilizada</th>"+
+        "<th data-hide=\"phone\">Estado</th>"+
+
+    "</tr>"+
+    "</thead>"+
+    "<tbody>"
+
+    this.info.forEach(item=>{
+      codigo+="<tr>"
+      codigo +="<td>"
+      codigo += item.pid
+      codigo +="</td>"
+      codigo +="<td>"
+      codigo += item.nombre
+      codigo +="</td>"
+      codigo += "<table class=\"table\"><thead><tr><td>PID</td><td>Nombre</td></tr></thead><tbody>"
+      item.hijos.forEach(hijo => {
+        codigo+="<tr>"
+        codigo+="<td>"
+        codigo+=hijo.pid
+        codigo+="</td>"
+        codigo+="<td>"
+        codigo+=hijo.nombre
+        codigo+="</td>"
+        codigo+="</tr>"
+      });
+      codigo+="</tbody></table>"
+      codigo +="<td>"
+      codigo += "user"
+      codigo +="</td>"
+      codigo +="<td>"
+      codigo += item.ram
+      codigo +="</td>"
+      codigo += "<td>"+item.estado+"</td></tr>"
+
+    })
+
+    codigo+="</tbody>"+
+    "<tfoot>"+
+    "<tr>"+
+        "<td colspan=\"6\">"+
+            "<ul class=\"pagination float-right\"></ul>"+
+        "</td>"+
+    "</tr>"+
+    "</tfoot>"+
+    "</table>"
+    jQuery('#tabla').html(codigo);
+    this.info = []
+    })
   }
-  
-  
+
+
   actualizar(){
       this.servicio.getProcesos().subscribe(resultado=>{
         //console.log(resultado.procesos)
@@ -39,7 +96,7 @@ export class ArbolprocesoComponent implements OnInit {
           rram1 = Number((rram1*100/2985.52).toFixed(3))
           rram = String(rram1)
           }
-          
+
           let item  ={
             nombre : element.nombre,
             pid : element.pid,
--- frontend/src/app/websocket.service.ts
+++ frontend/src/app/websocket.service.ts
@@ -3,10 +3,10 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 @Injectable()
 export class WebsocketService {
-  
+
   constructor(private http:HttpClient) { }
-  
-  url = "http://localhost:8080/"
+
+  url = "http://192.168.0.16:8080/"
 
   getInfoRam():Observable<any>{
     return this.http.get(this.url+"RAM");
