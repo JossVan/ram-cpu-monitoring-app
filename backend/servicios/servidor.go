ec1d051e21048576ed24a2bbaf835c43318010ed
jossvan <vanesitapolanco@gmail.com>
2021-12-14 20:23:24 -0600

    metodo para usuario

--- backend/servicios/servidor.go
+++ backend/servicios/servidor.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	//"log"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -13,11 +14,14 @@ import (
 func inicio(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Bienvenido... Proyecto 1")
 }
-func enableCors(w http.ResponseWriter) {
+func enableCors(w http.ResponseWriter, req *http.Request) {
 	(w).Header().Set("Access-Control-Allow-Origin", "*")
+	(w).Header().Set("Access-Control-Allow-Credentials", "true")
+	(w).Header().Set("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT")
+	(w).Header().Set("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
 }
 func infoRam(w http.ResponseWriter, r *http.Request) {
-	enableCors(w)
+	enableCors(w, r)
 	cmd := exec.Command("sh", "-c", "cat /proc/memo_201602676")
 	out, errorcito := cmd.CombinedOutput()
 	if errorcito != nil {
@@ -28,7 +32,7 @@ func infoRam(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, salida)
 }
 func procesos(w http.ResponseWriter, r *http.Request) {
-	enableCors(w)
+	enableCors(w, r)
 	cmd := exec.Command("sh", "-c", "cat /proc/cpu_201602676")
 	out, errorcito := cmd.CombinedOutput()
 	if errorcito != nil {
@@ -38,52 +42,56 @@ func procesos(w http.ResponseWriter, r *http.Request) {
 	salida := string(out[:])
 	io.WriteString(w, salida)
 }
-
-func kill(w http.ResponseWriter, r *http.Request) {
-	enableCors(w)
-	listapid, existen := r.URL.Query()["pid"]
-	// Imprimir para depurar
-	fmt.Printf("%v\n", listapid)
-	if existen {
-		cmd := exec.Command("sh", "-c", "kill -9 "+listapid[0])
+func usuario(w http.ResponseWriter, r *http.Request) {
+	enableCors(w, r)
+	decoder := json.NewDecoder(r.Body)
+	var params map[string]string
+	decoder.Decode(&params)
+	if len(params) != 0 {
+		comando := "getent passwd " + params["user"] + " | cut -d: -f1"
+		cmd := exec.Command("sh", "-c", comando)
 		out, errorcito := cmd.CombinedOutput()
 		if errorcito != nil {
-			log.Print(errorcito)
+			io.WriteString(w, "{\"values\":\"false\"}")
 			return
 		}
-		io.WriteString(w, string(out[:]))
-	} else {
+		salida := string(out[:])
+		salida = strings.Replace(salida, "\n", "", 2)
+		salida = "{\"values\":\"" + salida + "\"}"
 
-		io.WriteString(w, "efe")
+		io.WriteString(w, salida)
+	}
+}
+func kill(w http.ResponseWriter, r *http.Request) {
+	enableCors(w, r)
+	decoder := json.NewDecoder(r.Body)
+	var params map[string]string
+	decoder.Decode(&params)
+
+	if len(params) != 0 {
+		comando := "echo admin | sudo -S kill " + params["pid"]
+		cmd := exec.Command("sh", "-c", comando)
+		out, errorcito := cmd.CombinedOutput()
+		if errorcito != nil {
+			io.WriteString(w, "{ok:\"false\"}")
+			return
+		}
+		fmt.Println(string(out[:]))
+		io.WriteString(w, "{ok:\"true\"}")
 	}
 
 }
 func cpu(w http.ResponseWriter, r *http.Request) {
-	enableCors(w)
-	cmd := exec.Command("sh", "-c", "ps -eo pcpu | sort -k 1 -r | head -50")
+	enableCors(w, r)
+	cmd := exec.Command("sh", "-c", "top -bn 1 -i -c | head -n 3 | tail -1 | awk {'print $8'}")
 	out, errorcito := cmd.CombinedOutput()
 	if errorcito != nil {
 		fmt.Println(errorcito)
 		return
 	}
 	salida := string(out[:])
-	array := strings.Split(salida, "\n")
-	contador := 0
-	cadena := ""
-	for _, num := range array {
-		contador = contador + 1
-		if contador == 1 {
-			cadena += "\"" + num + "\":{\n"
-		} else {
-			cadena += num
-			if contador != (len(array) - 1) {
-				cadena += ",\n"
-			}
-		}
-
-	}
-	cadena += "}"
-	io.WriteString(w, cadena)
+	salida = strings.Replace(salida, "\n", "", 2)
+	io.WriteString(w, "{\"total\":\""+salida+"\"}")
 }
 func setupRoutes() {
 	http.HandleFunc("/", inicio)
@@ -91,6 +99,7 @@ func setupRoutes() {
 	http.HandleFunc("/procesos", procesos)
 	http.HandleFunc("/kill", kill)
 	http.HandleFunc("/cpu", cpu)
+	http.HandleFunc("/user", usuario)
 }
 
 func main() {
